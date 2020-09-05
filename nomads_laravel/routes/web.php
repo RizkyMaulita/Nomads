a0c25fc9b622f7f9813c9d011a59bb3d6fef6a8f
RizkyMaulita <rizky.maulita@gmail.com>
2020-09-05 15:18:50 +0700

    CRUD Gallery done

--- nomads_laravel/routes/web.php
+++ nomads_laravel/routes/web.php
@@ -38,6 +38,8 @@ Route::prefix('admin')
             ->name('dashboard');
 
         Route::resource('travel-package','TravelPackageController');
+        Route::resource('gallery','GalleryController');
+
     });
 
 
